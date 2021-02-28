5c81a8c115c2e11a31882be045e3103c02dfff0b
Anh Doan <anh.doan@firstpage.vn>
2021-03-01 02:13:47 +0700

    Pagination data

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const Employee = require('../models/employee');
 
 const router = express.Router();
@@ -8,12 +9,61 @@ router.get('/', (req, res) => {
 });
 
 router.get('/get-list', (req, res) => {
+    const itemPerPage = 20;
+    const page = 1;
     Employee.find({})
-        .limit(1500)
+        .skip((itemPerPage * page) - itemPerPage)
+        .limit(itemPerPage)
         .select({ _id: 0 })
         .exec()
         .then((result) => {
-            res.render('index', { data: result });
+            Employee.countDocuments((err, count) => {
+                if (err) {
+                    res.status(500).json({ message: 'Something went wrong!' });
+                }
+                res.render('index', {
+                    data: result,
+                    current: page,
+                    num: 1,
+                    pages: Math.ceil(count / itemPerPage),
+                    rootLink: '/api/get-list/',
+                });
+
+                // res.render('index', { data: result });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(503).json({ status: 503, message: 'Service Unavailable. Please contact admin for support!' });
+        });
+});
+
+router.get('/get-list/:page', (req, res) => {
+    if (req.params.page === 1) {
+        res.redirect('/api/get-list');
+    }
+    const itemPerPage = 20;
+    const page = req.params.page || 1;
+    Employee.find({})
+        .skip((itemPerPage * page) - itemPerPage)
+        .limit(itemPerPage)
+        .select({ _id: 0 })
+        .exec()
+        .then((result) => {
+            Employee.countDocuments((err, count) => {
+                if (err) {
+                    res.status(500).json({ message: 'Something went wrong!' });
+                }
+                res.render('index', {
+                    data: result,
+                    current: page,
+                    num: (page === 1) ? 1 : ((page * itemPerPage) - itemPerPage) + 1,
+                    pages: Math.ceil(count / itemPerPage),
+                    rootLink: '/api/get-list/',
+                });
+
+                // res.render('index', { data: result });
+            });
         })
         .catch((err) => {
             console.log(err);
@@ -27,10 +77,26 @@ router.post('/add-employee', (req, res) => {
     //     .then((result) => res.send(result))
     //     .catch((err) => console.log(err));
     employee.save()
-        .then((result) => res.status(201).json({ status: 200, message: 'Successfully Added' }))
+        .then(() => res.status(201).json({ status: 200, message: 'Successfully Added' }))
         .catch((err) => {
             console.log(err);
             res.json({ message: 'Something went wrong!' });
         });
 });
+
+router.get('/add-dummy', (req, res) => {
+    for (let i = 0; i < 10; i += 1) {
+        const employee = new Employee({
+            name: faker.fake('{{name.firstName}} {{name.lastName}}'),
+            role: 'Designer',
+            department: 'Product 3',
+            team: 'Design',
+        });
+        employee.save((err) => {
+            if (err) { console.log(err); }
+        });
+    }
+    res.redirect('/api');
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -11,17 +11,20 @@ app.set('view engine', 'ejs');
 // Middlewares
 app.use(helmet());
 app.use(express.json({ limit: '300kb' }));
-app.use(express.urlencoded({ extended: false, limit: '100kb' }));
+app.use(express.urlencoded({ extended: false, limit: '300kb' }));
 
 app.use('/api/', mainRouter);
 
 app.use((req, res) => {
     res.status(404).json({ status: 404, message: 'API not found' });
 });
+
 // Connect MongoDB
-// Because this is a test. So I put MONGO_URI in .env.example
+// Because this is a test. So I put MONGO_URI in .env.example instead .env
+// To let you know MongoDb connection string.
+// Change .env.example into .env
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected Successfully'))
+    .then(() => console.log('Successfully connected to DB'))
     .catch((err) => {
         console.log(err);
     });
